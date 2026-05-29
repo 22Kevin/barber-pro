0851891fcb350ceb54fcf9cc4235a0099bb38515
Kevin <kevin@barberpro.dev>
2026-05-29 16:39:32 +0000

    chore: add versionCode: 1 to android config (required for Play Store)

--- app.config.ts
+++ app.config.ts
@@ -48,6 +48,7 @@ const config: ExpoConfig = {
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
     package: env.androidPackage,
+    versionCode: 1,
     permissions: ["POST_NOTIFICATIONS"],
     intentFilters: [
       {
