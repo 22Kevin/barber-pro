756aa514ff69604b83a09ec9d9451a65b511379f
Kevin <kevin.rayan25@gmail.com>
2026-06-24 11:23:54 +0000

    chore: google-services.json com mobilesdk_app_id real e dois SHA-1

--- create-google-services.cjs
+++ create-google-services.cjs
@@ -5,12 +5,12 @@ const content = {
   "project_info": {
     "project_number": "398675034994",
     "project_id": "barber-pro-488313",
-    "storage_bucket": "barber-pro-488313.appspot.com"
+    "storage_bucket": "barber-pro-488313.firebasestorage.app"
   },
   "client": [
     {
       "client_info": {
-        "mobilesdk_app_id": "1:398675034994:android:barberprodebug",
+        "mobilesdk_app_id": "1:398675034994:android:fdce2465589528eaf7d3eb",
         "android_client_info": {
           "package_name": "com.usebarberpro.app"
         }
@@ -39,12 +39,24 @@ const content = {
       ],
       "api_key": [
         {
-          "current_key": ""
+          "current_key": "AIzaSyBqMw1LF6FFRfb7ENHZIjXzgN6mj_nDAyA"
         }
       ],
       "services": {
         "appinvite_service": {
-          "other_platform_oauth_client": []
+          "other_platform_oauth_client": [
+            {
+              "client_id": "398675034994-8do72mahbndf5olf7rt4njteprr4vh0i.apps.googleusercontent.com",
+              "client_type": 3
+            },
+            {
+              "client_id": "398675034994-ko29a2ku5oos6kuehvs0jjt90u06hj0v.apps.googleusercontent.com",
+              "client_type": 2,
+              "ios_info": {
+                "bundle_id": "com.usebarberpro.app"
+              }
+            }
+          ]
         }
       }
     }
